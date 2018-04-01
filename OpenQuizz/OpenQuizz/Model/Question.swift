7aba7d95f3ca3a0ec0dce3c3e7f27ef82605b0a2
Vincent BRAY <vincentbray@free.fr>
2018-04-01 15:56:45 +0200

    Adapt Project to MVC Pattern

---
+++ OpenQuizz/OpenQuizz/Model/Question.swift
@@ -0,0 +1,10 @@
+//
+//  Question.swift
+//  OpenQuizz
+//
+//  Created by Vincent BRAY on 01/04/2018.
+//  Copyright © 2018 Vincent BRAY. All rights reserved.
+//
+
+import Foundation
+class Question
