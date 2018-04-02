ac792da92b226133a840ffa5a51783147958f712
Vincent BRAY <vincentbray@free.fr>
2018-04-02 15:17:53 +0200

    Create Class QuestionView Connect Storyboard with QuestionView.swift

---
+++ OpenQuizz/OpenQuizz/View/QuestionView.swift
@@ -0,0 +1,15 @@
+//
+//  QuestionView.swift
+//  OpenQuizz
+//
+//  Created by Vincent BRAY on 02/04/2018.
+//  Copyright © 2018 Vincent BRAY. All rights reserved.
+//
+
+import UIKit
+
+class QuestionView: UIView {
+
+    @IBOutlet private var label: UILabel!
+    @IBOutlet private var icon: UIImageView!
+}
