69d96661a202168b93b734d08c352136b1b31606
Vincent BRAY <vincentbray@free.fr>
2018-04-03 18:49:33 +0200

    Transform View

--- OpenQuizz/OpenQuizz/Controller/ViewController.swift
+++ OpenQuizz/OpenQuizz/Controller/ViewController.swift
@@ -55,7 +55,7 @@ class ViewController: UIViewController {
         if game.state == .ongoing {
             switch sender.state {
             case .began, .changed:
-                transformaQuestionViewWith(gesture: sender)
+                transformQuestionViewWith(gesture: sender)
             case .cancelled, .ended:
                 answerQuestion()
             default:
@@ -65,12 +65,47 @@ class ViewController: UIViewController {
         
     }
     
-    private func transformaQuestionViewWith(gesture: UIPanGestureRecognizer) {
+    private func transformQuestionViewWith(gesture: UIPanGestureRecognizer) {
+        let translation = gesture.translation(in: questionView)
+        let translationTransform = CGAffineTransform(translationX: translation.x, y: translation.y)
         
+        let screenWidth = UIScreen.main.bounds.width
+        let translationPercent = translation.x / (screenWidth/2)
+        let rotationAngle = (CGFloat.pi / 6) * translationPercent
+        
+        let rotationTransform = CGAffineTransform(rotationAngle: rotationAngle)
+        
+        let transform = translationTransform.concatenating(rotationTransform)
+        questionView.transform = transform
+        
+        if translation.x > 0 {
+            questionView.style = .correct
+        } else {
+            questionView.style = .incorrect
+        }
     }
     
     private func answerQuestion() {
+        switch questionView.style {
+        case .correct:
+            game.answerCurrentQuestion(with: true)
+        case .incorrect:
+            game.answerCurrentQuestion(with: false)
+        case .standard:
+            break
+        }
         
+        scoreLabel.text = "\(game.score) / 10"
+        
+        questionView.transform = .identity
+        questionView.style = .standard
+        
+        switch game.state {
+        case .ongoing:
+             questionView.title = game.currentQuestion.title
+        case .over:
+            questionView.title = "Game Over"
+        }
     }
     
 }
